3f19dfb58189284a7dec5e66f420e760dfd6d2fb
GianlucaCanova <gianluca3f@gmail.com>
2018-10-16 15:44:11 +0200

    class registerController quasi finita

--- Cinema/Register/RegisterViewController.swift
+++ Cinema/Register/RegisterViewController.swift
@@ -10,15 +10,59 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
-    
-    
+    enum TextFieldsType : Int{
+        case name = 0
+        case surname
+        case email
+        case password
+        case password2
+    }
+    private var listPerson : [Person]!
 
     @IBOutlet var registerCollection: [UITextField]!
     @IBAction func registerActionButton(_ sender: UIButton) {
+        for tag in 0...4 {
+            NSLog("tag: "+String(tag))
+            guard registerCollection[tag].text != "" else{
+                NSLog("campi vuoti")
+                return
+            }
+        }
+        NSLog("campi non vuoti")
+        var pw : String!
+        guard ( pw=registerCollection[TextFieldsType.password.rawValue].text ) == registerCollection[TextFieldsType.password2.rawValue].text else{
+            NSLog("password diverse")
+            return
+        }
+        NSLog("password uguali")
+        guard pw.count > 5 else{
+            NSLog("password minore di 6 car")
+            return
+        }
+        NSLog("password.count OK!")
+        let email = registerCollection[TextFieldsType.email.rawValue].text
+        for persona in listPerson {
+            guard persona.email != email else{
+                let alert = UIAlertController(title: "Attenzione", message: "Email già presente", preferredStyle: .alert)
+                let okay = UIAlertAction(title: "Okay", style: .cancel, handler: nil)
+                alert.addAction(okay)
+                self.present(alert, animated: true, completion: nil)
+                NSLog("email già esistente")
+                return
+            }
+            
+            
+            
+        }
+        NSLog("email non esistente, OK, registrato!")
+        self.performSegue(withIdentifier: "returnLoginSegue", sender: self)
+        
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        listPerson = Person.all()
+        
         // Do any additional setup after loading the view.
     }
     
