d715467877bfb15dabacba91706e3f532fad2291
Federico Ceretta <federicoceretta@Micheles-Mac.local>
2018-10-16 14:51:25 +0200

    FilmViewController NON FUNZIONANTE

--- Cinema/Models/FilmListViewController.swift
+++ Cinema/Models/FilmListViewController.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2018 Michele Pertile. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FilmListController: UIViewController {
+    
+    private var listOfFilms : [Film] = []
+    
+    var seat1 = Seat(seatButton: "", seatPerson: email.Person)
+    
+    listOfFilms.add(Film(title : "Due Uomini E Una Gamba", summary : "Gian", seats: seat1 , image : "tre_uomini_e_una_gamba"))
+}
