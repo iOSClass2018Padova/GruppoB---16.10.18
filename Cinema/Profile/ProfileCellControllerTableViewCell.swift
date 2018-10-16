74e7123d01ab4c188354a062dd524b2f2479159f
Giacomo Caviola <giacomo.caviola@gmail.com>
2018-10-16 08:41:56 -0700

    aggiunta profile class

---
+++ Cinema/Profile/ProfileCellControllerTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  ProfileCellControllerTableViewCell.swift
+//  Cinema
+//
+//  Created by Giacomo on 16/10/18.
+//  Copyright © 2018 Michele Pertile. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCellControllerTableViewCell: UITableViewCell {
+    
+    
+    static let kIdentifier = "DetailPersonInfoCellTop"
+    @IBOutlet weak var imageProfile: UIImageView!
+    @IBOutlet var profileText: [UITextField]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+
+
+class ProfileCellControllerTableViewCell2: UITableViewCell {
+    static let kIdentifier = "DetailPersonInfoCellBot"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
