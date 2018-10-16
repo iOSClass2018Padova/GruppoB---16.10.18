aec59291c4d5647e75a245743b9541ffb284bbe8
Michele <michele.pertile@tecnicosuperiorekennedy.it>
2018-10-16 09:44:58 +0200

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Michele Pertile on 16/10/2018.
+//  Copyright © 2018 Michele Pertile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
